a422301353c478a3b44743e0c89306b53dbda1cb
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-12-05 15:00:58 +0100

    map of poland - working

--- src/script.js
+++ src/script.js
@@ -24,3 +24,28 @@ $(document).ready(function () {
     });
   });
 });
+
+const regions = [
+  document.querySelector('#podkarpackie'),
+  document.querySelector('#malopolskie'),
+  document.querySelector('#slaskie'),
+  document.querySelector('#opolskie'),
+  document.querySelector('#dolnoslaskie'),
+  document.querySelector('#swietokrzyskie'),
+  document.querySelector('#lubelskie'),
+  document.querySelector('#lodzkie'),
+  document.querySelector('#mazowieckie'),
+  document.querySelector('#wielkopolskie'),
+  document.querySelector('#lubuskie'),
+  document.querySelector('#kujawsko-pomorskie'),
+  document.querySelector('#podlaskie'),
+  document.querySelector('#zachodnio-pomorskie'),
+  document.querySelector('#warminsko-mazurskie'),
+  document.querySelector('#pomorskie'),
+];
+
+regions.forEach(element => {
+  element.addEventListener('click', function(){
+    console.log(element.id)
+  })
+});
