e1c747255f0054cfa1f3eff591700cb0216f62e9
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-12-17 01:27:21 +0100

    dataTables.js is working, data imported and sorted

--- src/script.js
+++ src/script.js
@@ -1,51 +1,50 @@
-$(document).ready(function () {
-  $("#load_data").click(function () {
-    $.ajax({
-      url: "jeczmien_jary.csv",
-      dataType: "text",
-      success: function (data) {
-        let employee_data = data.split(/\r?\n|\r/);
-        let table_data = '<table class="">';
-        for (let count = 0; count < employee_data.length; count++) {
-          let cell_data = employee_data[count].split(";");
-          table_data += "<tr>";
-          for (
-            let cell_count = 0;
-            cell_count < cell_data.length;
-            cell_count++
-          ) {
-            table_data += "<td>" + cell_data[cell_count] + "</td>";
-          }
-          table_data += "</tr>";
-        }
-        table_data += "</table>";
-        $("#employee_table").html(table_data);
+const table = new DataTable("#jeczmien_jary", {
+  ajax: "jeczmien_jary.json",
+  lengthMenu: [
+    [10, -1],
+    ["Tylko najlepsze", "Wszystkie"],
+  ],
+  //by pkazać wszystko na raz
+  // lengthChange: false,
+  // pageLength: -1,
+  language: {
+    lengthMenu: "_MENU_ wyniki na stronie",
+    search: `Rok: <select id="yearFilter" class="w-16 h-8">
+          <option value="2023" selected>2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>`,
+    info: "_START_-_END_ z _TOTAL_ wyników",
+    infoFiltered: "",
+  },
+
+  columnDefs: [
+    {
+      target: [1, 2, 3, 4, 8, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+      visible: false,
+    },
+    {
+      target: 30,
+      render: function () {
+        return '<button class="flex justify-center w-full hover:text-top-agrar-green"><i class="demo-icon icon-balance-scale">&#xf24e</i></button>';
       },
-    });
-  });
+    },
+  ],
 });
+const elementYearFilter = document.querySelector("#yearFilter");
+elementYearFilter.addEventListener("change", filtrYear);
+filtrYear();
 
-const regions = [
-  document.querySelector('#podkarpackie'),
-  document.querySelector('#malopolskie'),
-  document.querySelector('#slaskie'),
-  document.querySelector('#opolskie'),
-  document.querySelector('#dolnoslaskie'),
-  document.querySelector('#swietokrzyskie'),
-  document.querySelector('#lubelskie'),
-  document.querySelector('#lodzkie'),
-  document.querySelector('#mazowieckie'),
-  document.querySelector('#wielkopolskie'),
-  document.querySelector('#lubuskie'),
-  document.querySelector('#kujawsko-pomorskie'),
-  document.querySelector('#podlaskie'),
-  document.querySelector('#zachodnio-pomorskie'),
-  document.querySelector('#warminsko-mazurskie'),
-  document.querySelector('#pomorskie'),
-];
+function filtrYear() {
+  const selectedYear = elementYearFilter.value;
+  table.columns(1).search(selectedYear).draw();
+}
 
-regions.forEach(element => {
-  element.addEventListener('click', function(){
-    console.log(element.id)
-  })
+const regions = [document.querySelector("#podkarpackie"), document.querySelector("#malopolskie"), document.querySelector("#slaskie"), document.querySelector("#opolskie"), document.querySelector("#dolnoslaskie"), document.querySelector("#swietokrzyskie"), document.querySelector("#lubelskie"), document.querySelector("#lodzkie"), document.querySelector("#mazowieckie"), document.querySelector("#wielkopolskie"), document.querySelector("#lubuskie"), document.querySelector("#kujawsko-pomorskie"), document.querySelector("#podlaskie"), document.querySelector("#zachodnio-pomorskie"), document.querySelector("#warminsko-mazurskie"), document.querySelector("#pomorskie")];
+
+regions.forEach((element) => {
+  element.addEventListener("click", function () {
+    console.log(element.id);
+  });
 });
