443d47221cd9aa4082cfa211ef4417b2214968a1
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2025-01-16 07:26:21 +0100

    compare style desktop

--- src/script/compareScript.js
+++ src/script/compareScript.js
@@ -1,3 +1,6 @@
+document.querySelector('#prev').classList.add('hidden')
+document.querySelector('#next').classList.add('hidden')
+
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("compare")) {
     let id = event.target.id.replace("-button", "").replace("-span", "");
@@ -32,33 +35,49 @@ class Compare {
 
     this.rowsToCompare = [];
     this.element = document.querySelector(`#${elementId}`);
+    this.scrollPosition = 0;
   }
-
   //wyświetlanie porównywarki
   displayCompare() {
-    if (this.rowsToCompare.length === 0) {
+    if (this.rowsToCompare.length < 2) {
+      document.querySelector('#prev').classList.add('hidden')
+      document.querySelector('#next').classList.add('hidden')
       this.element.innerHTML = `<p class="text-2xl text-top-agrar-green text-center">Dodaj odmianę do porównania - kliknij ikonę<i class="icon-balance-scale pr-2 pl-1"></i>przy odmianie</p>`;
-    } else {
-      let table = '<div class=" flex compare-container justify-center ">';
+    } else {    
+      document.querySelector('#prev').classList.remove('hidden')
+      document.querySelector('#next').classList.remove('hidden')
 
-      table += '<div class="compare-header flex flex-col">';
+      let table = '<div class=" flex compare-container justify-center ">';
+      let colsCounter = 0;
+      table += '<div class="compare-header">';
       for (let cell of this.colNames) {
         table += `<div class="compare-name compare-cell">${cell}</div>`;
       }
       table += '</div><div class="scrolable-container">';
 
       for (let i = 0; i < this.rowsToCompare.length; i += 1) {
-        table += '<div class="compare-column flex flex-col">';
+        table += '<div class="compare-column">';
+        colsCounter += 1;
         for (let j = 0; j < this.rowsToCompare[i].length - 1; j += 1) {
           table += `<div class="compare-cell">${this.rowsToCompare[i][j]}</div>`;
         }
-        table += "</div>";
+        table += '</div>'
       }
-
       table += "</div></div>";
+
       this.element.innerHTML = table;
-      console.log(this.rowsToCompare);
+      
+      document.querySelectorAll('.compare-column').forEach((column) => {
+        column.style.width = (screen.width*8/12 - 204)/colsCounter + "px";
+      });
     }
+    document.querySelector('#prev').removeEventListener('click', this.scrollLeftHandler);
+    document.querySelector('#next').removeEventListener('click', this.scrollRightHandler);
+
+    this.scrollLeftHandler = () => this.scroll('left');
+    this.scrollRightHandler = () => this.scroll('right');
+    document.querySelector('#prev').addEventListener('click', this.scrollLeftHandler);
+    document.querySelector('#next').addEventListener('click', this.scrollRightHandler);
   }
 
   //dodawanie wiersza do porównywarki
@@ -77,4 +96,23 @@ class Compare {
     }
     this.displayCompare();
   }
+
+  scroll(direction){
+    const items = document.querySelectorAll('.compare-column');
+
+    if(direction === "left"){
+      this.scrollPosition -= 160;
+    } else {
+      this.scrollPosition += 160;
+    }
+  
+    const maxScroll = (items.length * 160) - (screen.width * 8 / 12 - 204);
+    if (this.scrollPosition < 0) this.scrollPosition = 0;
+    if (this.scrollPosition > maxScroll) this.scrollPosition = maxScroll;
+
+    items.forEach((item) => {
+      item.style.transform = `translateX(-${this.scrollPosition}px)`;
+    });
+  }
 }
+
